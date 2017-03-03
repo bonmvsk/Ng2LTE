f0535f11c61a8d53e440ce4d8512bf1855661426
Operation_X <bonmvsk@gmail.com>
2017-03-03 23:14:07 +0700

    update framework

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { LocationStrategy, HashLocationStrategy } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
+import { MaterialModule } from '@angular/material';
+import 'hammerjs';
 
+import { AppRoutingModule } from './app.routes';
+import { FooterModule } from './widgets/footer.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './pages/login/login.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule,
+    AppRoutingModule,
+    FooterModule,
+    [MaterialModule]
   ],
-  providers: [],
+  providers: [
+    [Location, { provide: LocationStrategy, useClass: HashLocationStrategy }],
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
